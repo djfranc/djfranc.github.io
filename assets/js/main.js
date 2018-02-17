65aa6a0d5277f39432e4f009287ad2ce07d66d46
djfranc <djamil.francis@gmail.com>
2018-02-17 17:43:00 -0500

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener(function() {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
@@ -18,3 +18,28 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 }
+
+/*
+function popout(){
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+      /*  this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+      /*  var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+}
+
+
+*/
